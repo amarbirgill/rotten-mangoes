0d71f58b76de061a99ac983d4c5714d4c38d8fd4
amarbirgill <amarbir_151@hotmail.com>
2015-06-08 18:32:50 -0700

    create user table and add validations to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515210516) do
+ActiveRecord::Schema.define(version: 20150609013054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20150515210516) do
     t.datetime "release_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
