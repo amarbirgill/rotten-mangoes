327b4dde011e7dbf2fe513ebafa5e7e0ff593227
amarbirgill <amarbir_151@hotmail.com>
2015-06-17 19:27:42 -0700

    add admin column to users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :movies do 
     resources :reviews, only: [:new, :create]
   end
+  namespace :admin do 
+    resources :users, only: [:new, :create, :update]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
