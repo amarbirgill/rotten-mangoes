ec2f09e6b47c54d1d6b98f5fea739ea3accba114
amarbirgill <amarbir_151@hotmail.com>
2015-06-21 18:32:04 -0700

    simple search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
+ 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    @movies = recent_or_search
   end
 
   def show
@@ -39,6 +41,27 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to movies_path
+  end
+
+  def recent_or_search
+    relation = Movie.all
+    relation = 
+    if params[:search]
+      search = params[:search]
+      relation = relation.where(director: search[:director]) unless search[:director].empty?
+      relation = relation.where(title: search[:title]) unless search[:title].empty?
+      if !search[:duration].empty? then
+        runtime = case search[:duration]
+        when '<=90' then '<= 90'
+        when '90><120' then 'between 90 and 120'
+        else '>=120'
+        end
+        relation = relation.where("runtime_in_minutes #{runtime}")
+      end
+      relation
+    else
+      relation.order(:created_at).limit(5)
+    end
   end
 
   protected
